bb0c6a61f150ee6a5299004c29ef870a65da5034
Souvick Saha <souvicksaha95@gmail.com>
2020-05-16 12:12:13 +0530

    Create shiftreg.v

---
+++ shiftreg.v
@@ -0,0 +1,12 @@
+module shiftreg (data_in, data_out,s_in, clk, ld, clr, sft);
+  input s_in, clk, ld, clr, sft;
+  input [15:0] data_in;
+  output reg [15:0] data_out;
+  
+  always @ (posedge clk)
+    begin
+      if (clr) data_out <= 0;
+      else if (ld) data_out <= data_in;
+      else if (sft) data_out <= {s_in, data_out[15:0]};
+    end
+endmodule
